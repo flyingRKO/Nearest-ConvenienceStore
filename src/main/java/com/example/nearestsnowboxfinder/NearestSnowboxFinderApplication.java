91b0d75c04e98976bc63b628a95b6a8f9831ee0e
flyingRKO <wogns970@naver.com>
2023-08-08 22:20:29 +0900

    Initial commit

---
+++ src/main/java/com/example/nearestsnowboxfinder/NearestSnowboxFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.nearestsnowboxfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearestSnowboxFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NearestSnowboxFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nearestsnowboxfinder/NearestSnowboxFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nearestsnowboxfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearestSnowboxFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

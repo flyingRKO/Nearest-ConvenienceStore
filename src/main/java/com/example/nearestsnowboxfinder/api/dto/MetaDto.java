aee3dd27556637d52596c6c2d90419448c2e6452
Lee Jae Hun <wogns970@naver.com>
2023-08-10 01:01:42 +0900

    kakao 주소검색 api 구현

---
+++ src/main/java/com/example/nearestsnowboxfinder/api/dto/MetaDto.java
@@ -0,0 +1,15 @@
+package com.example.nearestsnowboxfinder.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MetaDto {
+
+    @JsonProperty("total_count")
+    private String totalCount;
+}
---
+++ src/main/java/com/example/nearestsnowboxfinder/api/service/KaKaoAddressSearchService.java
@@ -0,0 +1,39 @@
+package com.example.nearestsnowboxfinder.api.service;
+
+import com.example.nearestsnowboxfinder.api.dto.KakaoApiResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KaKaoAddressSearchService {
+
+    private final RestTemplate restTemplate;
+    private final KakaoUriBuilderService kakaoUriBuilderService;
+
+    @Value("${KAKAO_REST_API_KEY}")
+    private String kakaoRestApiKey;
+
+    public KakaoApiResponseDto requestAddressSearch(String address) {
+        if (ObjectUtils.isEmpty(address)) return null;
+
+        URI uri = kakaoUriBuilderService.buildUriByAddressSearch(address);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "KakaoAK " + kakaoRestApiKey);
+        HttpEntity httpEntity = new HttpEntity<>(headers);
+
+        // call kakao api
+        return restTemplate.exchange(uri, HttpMethod.GET, httpEntity, KakaoApiResponseDto.class).getBody();
+    }
+}

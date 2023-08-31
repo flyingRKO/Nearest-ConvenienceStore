18ef191df0f6bfec6a7a1c721dcbb5218a0c0603
flyingRKO <wogns970@naver.com>
2023-09-01 02:09:15 +0900

    Redis를 이용한 성능 개선에 불필요한 부분 제거

--- src/main/java/com/example/nearestconveniencestore/config/RedisConfig.java
+++ src/main/java/com/example/nearestconveniencestore/config/RedisConfig.java
@@ -27,8 +27,7 @@ public class RedisConfig {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
 }
